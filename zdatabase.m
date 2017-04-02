189983dd5c877c86b3a7be9ea1993139bd5fc21c
Leo Pauly <meetleopauly@yahoo.com>
2017-04-02 16:38:11 +0100

    Add files via upload

---
+++ zdatabase.m
@@ -0,0 +1,15 @@
+clc
+clear all
+close all
+imagefiles=dir('*.png')
+num=124
+for i=1:130
+    imagename=imagefiles(i).name
+    image=imread(imagename);
+    image=imresize(image,[99,99]);
+    figure,imshow(image);
+    c=int2str(num);
+    newname=strcat('./',c,'.jpg');
+    num=num+1;
+    imwrite(image,newname);
+end
