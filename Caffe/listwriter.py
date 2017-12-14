47180ee2bc97566443d959d0686b73f84cf7068f
Leo Pauly <meetleopauly@yahoo.com>
2017-12-14 11:40:28 +0000

    Update listwriter.py

--- Caffe/listwriter.py
+++ Caffe/listwriter.py
@@ -7,4 +7,4 @@ for i in range (1,57):
     c=str(i)
     #print('train_new/input/'+c+'.jpg train_new/output_images/'+c+'.PNG')  # Segmentation (input and output are images)
     print('c ('+c+').PNG 0') # Classification (input images and output labels)
-    print('c ('+c+').PNG') # Testing (input images and no output)
+    #print('c ('+c+').PNG') # Testing (input images and no output)
